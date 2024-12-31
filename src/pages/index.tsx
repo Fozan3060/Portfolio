446733c69887a234bc5b71ffa52af1758b0db127
Fozan3060 <98460987+Fozan3060@users.noreply.github.com>
2024-12-30 16:50:13 -0800

    feat: intialized project and added header

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Header from "../components/complex/Header";
+
+const IndexPage: React.FC = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default IndexPage;
