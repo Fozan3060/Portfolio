845fad24f93dbe9591ba54aae46784e06c07d804
Fozan3060 <98460987+Fozan3060@users.noreply.github.com>
2025-01-01 22:10:20 -0800

    feat: integrated api in herosection and update tests

--- src/components/compound/ActionPanel.tsx
+++ src/components/compound/ActionPanel.tsx
@@ -1,26 +1,36 @@
 import React from "react";
-import { IoSunnyOutline } from "react-icons/io5";
 import Button from "../shared/Button";
+import ThemeChanger from "./ThemeChanger";
+import { useUserDetails } from "../Context/UserDetailsContext";
 
 const ActionPanel = () => {
+  const { cvDocument } = useUserDetails();
+
+  const handleDownloadCV = () => {
+    if (cvDocument?.asset?.url) {
+      const fileUrl = cvDocument.asset.url;
+      const fileName = fileUrl.split("/").pop() || "CV"; 
+      const link = document.createElement("a");
+      link.href = fileUrl;
+      link.download = fileName; 
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } else {
+      console.error("CV document URL is not available.");
+    }
+  };
+
   return (
     <div
       data-testid="ActionPanel"
       className="flex md:flex-row flex-col items-center gap-5"
     >
-      <Button
-        description=""
-        className="flex mt-5 md:mt-0 justify-between w-full items-center"
-        onClick={() => {}}
-        icon={""}
-      >
-        <span className="md:hidden block">Switch Theme</span>
-        <IoSunnyOutline size={26} />
-      </Button>
+      <ThemeChanger />
       <Button
         description="Download CV"
-        className="text-gray-50 w-full md:w-fit text-nowrap bg-gray-900 rounded-xl font-medium hover:bg-gray-700 transition-all duration-200 p-2 px-4"
-        onClick={() => {}}
+        className="text-gray-50 dark:text-gray-900 w-full md:w-fit text-nowrap dark:hover:bg-gray-300 dark:bg-gray-50 bg-gray-900 rounded-xl font-medium hover:bg-gray-700 transition-all duration-200 p-2 px-4"
+        onClick={handleDownloadCV}
         icon=""
       />
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     `./src/pages/**/*.{js,jsx,ts,tsx}`,
     `./src/components/**/*.{js,jsx,ts,tsx}`,
@@ -8,6 +9,10 @@ module.exports = {
     extend: {
       fontFamily: {
         inter: ['Inter', 'sans-serif'], 
+      },
+      boxShadow: {  
+        CustomboxShadow: "rgba(0, 0, 0, 0.2) 0px 4px 12px;"
+
       },
     },
   },
